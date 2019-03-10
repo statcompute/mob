e7ce4ac89c07e8be18402217dc87e5875f12efb1
WenSui Liu <liuwensui@gmail.com>
2019-03-10 14:00:20 -0500

    Create mono_bin.R
    The mono_bin() function implements the monotonic binning algorithm in the scorecard development.

---
+++ code/mono_bin.R
@@ -0,0 +1,35 @@
+### import an utility function manual_bin() ###
+source("https://raw.githubusercontent.com/statcompute/MonotonicBinning/master/code/manual_bin.R")
+
+mono_bin <- function(data, y, x) {
+  yname <- deparse(substitute(y))
+  xname <- deparse(substitute(x))  
+  df1 <- data[!is.na(data[, xname]), c(xname, yname)]  
+  nbin <- min(100, nrow(unique(df1[[xname]])))
+  cut2 <- function(x, g) {
+    c <- unique(quantile(x, probs = seq(0, 1, 1 / g), na.rm = T, names = F, type = 3))
+    return(c(0, rep(1, length(c) - 2), 0) + c)
+  }  
+  repeat {
+    cuts <- cut2(df1[[xname]], g = nbin)
+    df1$cut <- cut(df1[[xname]], breaks = cuts, include.lowest = T)    
+    df2 <- Reduce(rbind, 
+             Map(function(x) data.frame(xmean = mean(x[[xname]], na.rm = T), 
+                                        ymean = mean(x[[yname]], na.rm = T)), 
+               split(df1, df1$cut)))
+    if(abs(cor(df2$xmean, df2$ymean, method = "spearman")) == 1 | nrow(df2) == 2) {
+      break
+    }    
+    nbin <- nbin - 1
+  }
+  return(manual_bin(data, yname, xname, cuts = cuts[c(-1, -length(cuts))]))
+}
+
+# mono_bin(df, bad, ltv)
+#Bin                           Rule Frequency Percent MV_Cnt Bad_Freq Bad_Rate     WoE InfoValue KS_Stat
+# 01                       $X <= 86      1108    0.19      0      122   0.1101 -0.7337    0.0810   11.04
+# 02             $X > 86 & $X <= 95      1081    0.19      0      166   0.1536 -0.3510    0.0205   16.88
+# 03            $X > 95 & $X <= 101      1102    0.19      0      242   0.2196  0.0880    0.0015   15.18
+# 04           $X > 101 & $X <= 106       743    0.13      0      177   0.2382  0.1935    0.0050   12.57
+# 05           $X > 106 & $X <= 115       935    0.16      0      226   0.2417  0.2126    0.0077    8.95
+# 06           $X > 115 | is.na($X)       868    0.15      1      263   0.3030  0.5229    0.0468    0.00
