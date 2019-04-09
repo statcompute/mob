7c3caf0f4f9b908e57d64df36f8f84c5cc3e7abc
WenSui Liu <liuwensui@gmail.com>
2019-04-08 20:23:37 -0500

    Update batch_bin.R

--- code/batch_bin.R
+++ code/batch_bin.R
@@ -1,11 +1,22 @@
-batch_bin <- function(data, method) {
+batch_bin <- function(data, method = "mono_bin") {
+# INPUT
+# data  : input dataframe with Y in the last column
+# method: a character string indicating the method used for monotonic binning
+#         one of "mono_bin", "iso_bin", or "gbm_bin"
+# OUTPUT
+# batch_bin(df, "mono_bin")
+#          var nbin miss      ks     iv
+#    tot_derog    3  213 18.9469 0.1975
+#   ...SKIPPED...
+#   tot_income    6    5 10.2171 0.0677
 
+  ### GET THINGS READY ###
   df1 <- subset(data, data[, ncol(data)] %in% c(0, 1))
   xnames <- colnames(df1)[1:(ncol(df1) - 1)]
   yname <- colnames(df1)[ncol(df1)]
   bin_fn <- get(method)
 
-  xlst <- parallel::mclapply(xnames, mc.cores = parallel::detectCores(),
+  xlst <- parallel::mclapply(xnames, mc.cores = parallel::detectCores(), 
            function(xname) bin_fn(data.frame(y = df1[[yname]], x = df1[[xname]]), y, x))
   names(xlst) <- xnames
 
@@ -19,7 +30,6 @@ batch_bin <- function(data, method) {
   BinOut <- list(BinSum = xsum, BinLst = xlst)
   class(BinOut) <- "BinSummary"
   return(BinOut)
-  #return(list(BinSum = xsum, BinLst = xlst))
 }
 
 print.BinSummary <- function(x) {
