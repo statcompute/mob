a3b8f875b28afca974064cd7141ab0160e30e83b
WenSui Liu <liuwensui@gmail.com>
2019-04-20 15:56:31 -0500

    Update and rename calc_woe.R to cal_woe.R

--- code/cal_woe.R
+++ code/cal_woe.R
@@ -1,15 +1,11 @@
-calc_woe <- function(data, x, spec) {
-# data : the input dataframe
-# x    : the numeric variable to which the woe is applied
-# spec : the output table from binning functions, e.g. mono_bin() or iso_bin()
-# 
-# source("https://raw.githubusercontent.com/statcompute/MonotonicBinning/master/code/calc_woe.R")
+cal_woe <- function(data, xname, spec) {
+# INPUT
+# data  : the input dataframe
+# xname : name of the numeric variable to which the woe is applied
+# spec  : the output table from binning functions, e.g. mono_bin() or iso_bin()
 
-  ### GET THINGS READY ###
-  xname <- deparse(substitute(x))
   wname <- paste("woe", xname, sep = ".")
 
-  ### DEFINE A FUNCTION TO CALCULATE WOE ###
   calc <- function(i) {
     s <- spec[i, ]
     if (length(with(data, which(eval(parse(text = gsub("$X", xname, s$rule, fixed = T)))))) == 0) {
@@ -22,18 +18,23 @@ calc_woe <- function(data, x, spec) {
     }
   }
   df1 <- Reduce(rbind, Map(calc, seq(nrow(spec))))
-  
-  ### SUMMARIZE THE DATA BY EACH WOE CATEGORY ###
+
   sm1 <- Reduce(rbind,
-           Map(function(x) data.frame(bin      = unique(x$wbin),
+           Map(function(x) data.frame(bin      = unique(x$wbin), 
                                       cal_freq = nrow(x),
-                                      cal_dist = round(nrow(x) / nrow(df1), 4),
+                                      cal_dist = round(nrow(x) / nrow(df1), 4), 
                                       cal_woe  = mean(x[[wname]])),
-             split(df1, df1$wbin, drop = T)))  
-             
+             split(df1, df1$wbin, drop = T)))
+  
   ### MERGE THE SPEC TABLE AND THE WOE SUMMARY TABLE ###
   sm2 <- merge(spec[, c("bin", "rule", "dist", "woe")], sm1, by = c("bin"), all = T)
   sm2$psi <- round((sm2$cal_dist - sm2$dist) * log(sm2$cal_dist / sm2$dist), 4)
   df1$wbin <- NULL
-  return(list(df = df1, psi = sm2))
-} 
+  woe_out <- list(df = df1, psi = sm2)
+  class(woe_out) <- "psi"
+  return(woe_out)
+}
+
+print.psi <- function(x) {
+  print(x$psi)
+}
