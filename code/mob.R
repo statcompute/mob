fa3c1f33fb1b22a900f3029a8ade3c30ea98cfd9
WenSui Liu <liuwensui@gmail.com>
2019-04-21 15:48:16 -0500

    Update mob.R

--- code/mob.R
+++ code/mob.R
@@ -1,11 +1,9 @@
 
 ###############################################################################
-#
 # PACKAGE NAME: MOB (MONOTONIC OPTIMAL BINNING)
 # AUTHOR      : WENSUI LIU
 # DISCLAIMER  : THIS IS MY WEEKEND PROJECT AND NOT RELATED TO MY CURRENT WORK WITH MY EMPLOYER
 #               IT IS FREE (AS FREE BEER) TO USE AND DISTRIBUTE
-#
 ###############################################################################
 
 manual_bin <- function(df, yname, xname, cuts) {
