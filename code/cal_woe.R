6949072cdcd3554adecca5c6388470196dbadbbe
WenSui Liu <liuwensui@gmail.com>
2019-04-27 12:30:23 -0500

    Update cal_woe.R

--- code/cal_woe.R
+++ code/cal_woe.R
@@ -1,8 +1,8 @@
 cal_woe <- function(data, xname, spec) {
 # INPUT
-# data  : the input dataframe
-# xname : name of the numeric variable to which the woe is applied
-# spec  : the output table from binning functions, e.g. mono_bin() or iso_bin()
+# data : the input dataframe
+# x    : the numeric variable to which the woe is applied
+# spec : the output table from binning functions, e.g. mono_bin() or iso_bin()
 
   wname <- paste("woe", xname, sep = ".")
 
@@ -13,23 +13,22 @@ cal_woe <- function(data, xname, spec) {
     }
     else {
       d <- data[with(data, which(eval(parse(text = gsub("$X", xname, s$rule, fixed = T))))), ]
-      d$wbin <- s$bin
+      d$woe.bin <- s$bin
       return(within(d, assign(wname, s$woe)))
     }
   }
   df1 <- Reduce(rbind, Map(calc, seq(nrow(spec))))
 
   sm1 <- Reduce(rbind,
-           Map(function(x) data.frame(bin      = unique(x$wbin), 
+           Map(function(x) data.frame(bin      = unique(x$woe.bin), 
                                       cal_freq = nrow(x),
                                       cal_dist = round(nrow(x) / nrow(df1), 4), 
                                       cal_woe  = mean(x[[wname]])),
-             split(df1, df1$wbin, drop = T)))
+             split(df1, df1$woe.bin, drop = T)))
   
   ### MERGE THE SPEC TABLE AND THE WOE SUMMARY TABLE ###
   sm2 <- merge(spec[, c("bin", "rule", "dist", "woe")], sm1, by = c("bin"), all = T)
   sm2$psi <- round((sm2$cal_dist - sm2$dist) * log(sm2$cal_dist / sm2$dist), 4)
-  df1$wbin <- NULL
   woe_out <- list(df = df1, psi = sm2)
   class(woe_out) <- "psi"
   return(woe_out)
