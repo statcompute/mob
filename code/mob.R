f82eb83e19ef2eb86ce088c088cbf9986f604b2e
WenSui Liu <liuwensui@gmail.com>
2019-06-15 12:39:58 -0500

    Update mob.R

--- code/mob.R
+++ code/mob.R
@@ -118,28 +118,17 @@ qtl_bin <- function(data, y, x) {
   yname <- deparse(substitute(y))
   xname <- deparse(substitute(x))  
   df1 <- subset(data, !is.na(data[[xname]]) & data[[yname]] %in% c(0, 1), select = c(xname, yname))
-  nbin <- 2
- 
-  repeat {
-    pts <- unique(quantile(df1[[xname]], prob = seq(0, 1, 1 / (nbin + 1)), names = F, type = 3))
-    df1$cut <- cut(df1[[xname]], breaks = pts, include.lowest = T)
-    df2 <- Reduce(rbind, 
-             Map(function(x) data.frame(xmean = mean(x[[xname]]), 
-                                        ymean = mean(x[[yname]])), 
-               split(df1, df1$cut)))
-
-    flg1 <- ifelse(round(abs(cor(df2$xmean, df2$ymean, method = "spearman", use = "complete.obs")), 8) < 1, 1, 0)
-    flg2 <- ifelse(max(df2$ymean) == 1 | min(df2$ymean) == 0, 1, 0)
-
-    if((flg1 + flg2) > 0) {
-      cuts <- unique(quantile(df1[[xname]], prob = seq(0, 1, 1 / nbin), names = F, type = 3))
-      break
-    }
-    nbin <- nbin + 1
-  }
-
-  return(list(df   = manual_bin(data, yname, xname, cuts = cuts[2:(length(cuts) - 1)]), 
-              cuts = cuts[2:(length(cuts) - 1)]))
+  pt0 <- unique(Map(function(n_) unique(quantile(df1[[xname]], prob = seq(0, 1, 1 / n_), names = F, type = 3)), 
+                    2:min(15, length(unique(df1[[xname]])))))
+  pts <- pt0[which(Map(length, pt0) > 2)]
+  cut <- Map(function(p_) cut(df1[[xname]], breaks = p_, include.lowest = T, labels = F), pts)
+  mns <- Map(function(c_) Reduce(rbind, Map(function(d_) data.frame(xmn = mean(d_[[xname]]), ymn = mean(d_[[yname]])),
+                                            split(df1, c_))), cut)
+  chk <- Reduce(rbind, Map(function(s_) data.frame(scor = round(abs(cor(s_$ymn, s_$xmn, method = "spearman", use = "complete.obs")), 8),
+                                                   rcnt = nrow(s_), miny = min(s_$ymn), maxy = max(s_$ymn)), mns))
+  sel <- tail(subset(cbind(chk, pts = I(pts)), scor == 1 & miny > 0 & maxy < 1), 1)$pts[[1]]
+  return(list(df   = manual_bin(data, yname, xname, cuts = sel[2:(length(sel) - 1)]), 
+              cuts = sel[2:(length(sel) - 1)]))
 }
 
 ###############################################################################
@@ -154,28 +143,17 @@ bad_bin <- function(data, y, x) {
   xname <- deparse(substitute(x))
   df1 <- subset(data, !is.na(data[[xname]]) & data[[yname]] %in% c(0, 1), select = c(xname, yname))
   df2 <- subset(df1, df1[[yname]] == 1)
-  nbin <- 2
-
-  repeat {
-    pts <- unique(quantile(df2[[xname]], prob = seq(0, 1, 1 / (nbin + 1)), names = F, type = 3))
-    df1$cut <- cut(df1[[xname]], breaks = pts, include.lowest = T)
-    df3 <- Reduce(rbind, 
-             Map(function(x) data.frame(xmean = mean(x[[xname]]), 
-			                ymean = mean(x[[yname]])), 
-               split(df1, df1$cut)))
-
-    flg1 <- ifelse(round(abs(cor(df3$xmean, df3$ymean, method = "spearman", use = "complete.obs")), 8) < 1, 1, 0)
-    flg2 <- ifelse(max(df3$ymean) == 1 | min(df3$ymean) == 0, 1, 0)
-
-    if((flg1 + flg2) > 0) {
-      cuts <- unique(quantile(df2[[xname]], prob = seq(0, 1, 1 / nbin), names = F, type = 3))
-      break
-    }
-    nbin <- nbin + 1
-  }
-
-  return(list(df   = manual_bin(data, yname, xname, cuts = cuts[2:(length(cuts) - 1)]),
-              cuts = cuts[2:(length(cuts) - 1)]))
+  pt0 <- unique(Map(function(n_) unique(quantile(df2[[xname]], prob = seq(0, 1, 1 / n_), names = F, type = 3)), 
+                    2:min(50, length(unique(df2[[xname]])))))
+  pts <- pt0[which(Map(length, pt0) > 2)]
+  cut <- Map(function(p_) cut(df1[[xname]], breaks = p_, include.lowest = T, labels = F), pts)
+  mns <- Map(function(c_) Reduce(rbind, Map(function(d_) data.frame(xmn = mean(d_[[xname]]), ymn = mean(d_[[yname]])),
+                                            split(df1, c_))), cut)
+  chk <- Reduce(rbind, Map(function(s_) data.frame(scor = round(abs(cor(s_$ymn, s_$xmn, method = "spearman", use = "complete.obs")), 8),
+                                                   rcnt = nrow(s_), miny = min(s_$ymn), maxy = max(s_$ymn)), mns))
+  sel <- tail(subset(cbind(chk, pts = I(pts)), scor == 1 & miny > 0 & maxy < 1), 1)$pts[[1]]
+  return(list(df   = manual_bin(data, yname, xname, cuts = sel[2:(length(sel) - 1)]), 
+              cuts = sel[2:(length(sel) - 1)]))
 }
 
 ###############################################################################
