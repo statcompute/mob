2a1262a9faf4b056df34070000cf714de7bd5168
WenSui Liu <liuwensui@gmail.com>
2019-07-28 14:27:53 -0500

    Update batch_woe.R

--- code/batch_woe.R
+++ code/batch_woe.R
@@ -6,19 +6,22 @@ batch_woe <- function(data, slst) {
 
   if (length(intersect(names(slst), colnames(data))) == 0) {
     stop('The data frame and the woe spec do not match !')
-  } else { 
+  } else {
     xnames <- intersect(names(slst), colnames(data))
     spec <- slst[xnames]
     df1 <- cbind(idx_ = seq(nrow(data)), data[, xnames])
   }
-
-  woe <- parallel::mclapply(xnames, mc.cores = parallel::detectCores(),
-           function(xname) cal_woe(df1, xname, spec[[xname]]$df))
+  cl <- parallel::makeCluster(parallel::detectCores(), type = "PSOCK")
+  parallel::clusterExport(cl, c("df1", "spec", "cal_woe"), envir = environment())
+  woe <- parallel::parLapply(cl, xnames,
+                             function(xname) cal_woe(df1, xname, spec[[xname]]$df))
   names(woe) <- xnames
-  
+
   psi <- lapply(woe, function(x) x$psi)
-  df2 <- Reduce(merge, parallel::mclapply(woe, mc.cores = parallel::detectCores(),
-           function(x) x$df[order(x$df[["idx_"]]), c(1, ncol(x$df))]))
+  df2 <- Reduce(merge,
+                parallel::parLapply(cl, woe,
+                                    function(x) x$df[order(x$df[["idx_"]]), c(1, ncol(x$df))]))
+  parallel::stopCluster(cl)
   out <- list(psi = psi, df = df2)
   class(out) <- "psiSummary"
   return(out)
